95d9b6fd44760adda19add0ac51a65708ee02e2c
Mensur Medic <mensur_medic@premierinc.com>
2014-12-03 13:56:59 -0500

    Explain plan for PostgreSQL.

--- SqlEditor/Databases/PostgreSql/PostgreSqlExplainPlanData.cs
+++ SqlEditor/Databases/PostgreSql/PostgreSqlExplainPlanData.cs
@@ -8,14 +8,11 @@ namespace SqlEditor.Databases.PostgreSql
         public override IList<ExplainPlanColumn> GetColumns()
         {
             var list = new List<ExplainPlanColumn>();
-            list.Add(new ExplainPlanColumn("id", "Id"));
-            list.Add(new ExplainPlanColumn("select_type", "Select Type"));
-            list.Add(new ExplainPlanColumn("table", "Table"));
-            list.Add(new ExplainPlanColumn("possible_keys", "Possible Keys"));
-            list.Add(new ExplainPlanColumn("key", "Key"));
-            list.Add(new ExplainPlanColumn("ref", "Ref"));
-            list.Add(new ExplainPlanColumn("ref", "Rows"));
-            list.Add(new ExplainPlanColumn("Extra", "Extra"));
+            list.Add(new ExplainPlanColumn("Operation", "Operation"));
+            list.Add(new ExplainPlanColumn("Startup_Cost", "Startup Cost"));
+            list.Add(new ExplainPlanColumn("Total_Cost", "Total Cost"));
+            list.Add(new ExplainPlanColumn("Row_Count", "Row Count"));
+            list.Add(new ExplainPlanColumn("Width", "Width"));
             return list;
         }
     }
--- SqlEditor/Databases/PostgreSql/PostgreSqlExplainPlanGenerator.cs
+++ SqlEditor/Databases/PostgreSql/PostgreSqlExplainPlanGenerator.cs
@@ -1,22 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Text.RegularExpressions;
 using JetBrains.Annotations;
 using SqlEditor.DatabaseExplorer;
-using SqlEditor.Databases.MySql;
 using SqlEditor.ExplainPlan;
 
 namespace SqlEditor.Databases.PostgreSql
 {
     public class PostgreSqlExplainPlanGenerator : ExplainPlanGenerator
     {
+        private static readonly Regex _leadingSpaceRegex = new Regex(@"^\s+", RegexOptions.Compiled);
+        private static readonly Regex _costRowRegex = new Regex(@"^(?<OperationName>.+?)\(\s*cost\s*=\s*(?<InitialCost>[\d\.]+)\s*\.\.\s*(?<TotalCost>[\d\.]+)\s*rows\s*=\s*(?<Rows>[\d\.]+)\s*width\s*=\s*(?<Width>[\d\.]+)\s*\)\s*$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
         public override ExplainPlanData GetExplainPlan([NotNull] DatabaseConnection databaseConnection,
             [NotNull] string sql)
         {
             if (databaseConnection == null) throw new ArgumentNullException("databaseConnection");
             if (sql == null) throw new ArgumentNullException("sql");
 
-            var table = new DataTable();
+            DataTable table;
             using (var connection = databaseConnection.CreateNewConnection())
             {
                 connection.OpenIfRequired();
@@ -32,14 +36,69 @@ namespace SqlEditor.Databases.PostgreSql
             }
 
             // Parse table
-            var explainPlanData = new MySqlExplainPlanData();
-            var rootRow = table.Rows.Cast<DataRow>().ToList();
-            if (rootRow.Count == 0)
+            var resultTable = new DataTable();
+            resultTable.Columns.Add("Operation", typeof (string));
+            resultTable.Columns.Add("Startup_Cost", typeof(double));
+            resultTable.Columns.Add("Total_Cost", typeof(double));
+            resultTable.Columns.Add("Row_Count", typeof(long));
+            resultTable.Columns.Add("Width", typeof(long));
+            var explainPlanData = new PostgreSqlExplainPlanData();
+            var dataRows = table.Rows.Cast<DataRow>().ToList();
+            var rootRow = dataRows.FirstOrDefault();
+            if (rootRow == null)
             {
                 throw new Exception("Could not find a starting point in the execution plan.");
             }
-            explainPlanData.Rows.AddRange(table.Rows.Cast<DataRow>().Select(x => new ExplainPlanRow(x)));
+            var rootExplainPlanRow = ParseSingleExplainPlanRow(rootRow, resultTable.NewRow());
+            explainPlanData.Rows.Add(rootExplainPlanRow);
+            var levelMap = new Dictionary<int, ExplainPlanRow>();
+            var parent = rootExplainPlanRow;
+            for (var i = 1; i < dataRows.Count; i++)
+            {
+                var data = ((string) dataRows[i][0]).Replace("->", "  ");
+                var level = 0;
+                var match = _leadingSpaceRegex.Match(data);
+                if (match.Success)
+                {
+                    level = match.Value.Length;
+                }
+                // Remove non relevant subtree
+                levelMap = levelMap.Where(x => x.Key <= level).ToDictionary(x => x.Key, y => y.Value);
+
+                var row = ParseSingleExplainPlanRow(dataRows[i], resultTable.NewRow());
+                if (!levelMap.ContainsKey(level))
+                {
+                    levelMap.Add(level, parent);
+                    parent.ChildRows.Add(row);
+                }
+                else
+                {
+                    levelMap[level].ChildRows.Add(row);
+                }
+                parent = row;
+            }
             return explainPlanData;
+        }
+
+        private static ExplainPlanRow ParseSingleExplainPlanRow(DataRow rootRow, DataRow newRow)
+        {
+            var data = (string)rootRow[0];
+            data = data.Replace("->", string.Empty).Trim();
+            var match = _costRowRegex.Match(data);
+            if (match.Success)
+            {
+                var name = match.Groups["OperationName"].Value.Trim();
+                var initialCost = double.Parse(match.Groups["InitialCost"].Value.Trim());
+                var totalCost = double.Parse(match.Groups["TotalCost"].Value.Trim());
+                var rows = long.Parse(match.Groups["Rows"].Value.Trim());
+                var width = long.Parse(match.Groups["Width"].Value.Trim());
+                newRow.ItemArray = new object[] { name, initialCost, totalCost, rows, width };
+            }
+            else
+            {
+                newRow.ItemArray = new object[] { data, null, null, null, null };   
+            }
+            return new ExplainPlanRow(newRow);
         }
     }
 }
